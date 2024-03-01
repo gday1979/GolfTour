2130afa8eb063f82b8cd9b2cc395a78ad4b2f433
Zlatin Shkembov <97783032+gday1979@users.noreply.github.com>
2024-03-01 19:56:49 +0200

    implemented middleware

--- GolfTour.Common/GlobalConstants.cs
+++ GolfTour.Common/GlobalConstants.cs
@@ -4,6 +4,21 @@
     {
         public const string SystemName = "GolfTour";
 
+        public const string SystemEmail = "zshekembov@gmail.com";
+
         public const string AdministratorRoleName = "Administrator";
+
+        public const string AdministratorUserName = "Admin";
+
+        public const string AdministratorEmail = "zshkembov@gmail.com";
+        public const string AdministratorPassword = "123456";
+
+        public const string AdministratorFullName = "Zlatin Shekembov";
+
+        public const string UserRoleName = "User";
+
+        public const string AllowedExtensionsErrorMessage = "This photo extension is not allowed!";
+
+        public static readonly string[] AllowedImageExtensions = { ".jpg", ".png", ".jpeg", ".gif" };
     }
 }
---
+++ Web/GolfTour.Web/Middlewares/AdminMiddleware.cs
@@ -0,0 +1,47 @@
+﻿namespace GolfTour.Web.Middlewares
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Azure.Core;
+    using GolfTour.Common;
+    using GolfTour.Data.Models;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+
+    public class AdminMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public AdminMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, UserManager<GolfTourUser> userManager)
+        {
+            await this.SeedUserInRoles(userManager);
+            await this.next(context);
+        }
+
+        private async Task SeedUserInRoles(UserManager<GolfTourUser> userManager)
+        {
+            var user = new GolfTourUser
+            {
+                UserName = GlobalConstants.AdministratorUserName,
+                Email = GlobalConstants.AdministratorEmail,
+                FullName = GlobalConstants.AdministratorFullName,
+            };
+            var result= await userManager.CreateAsync(user, GlobalConstants.AdministratorPassword);
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(user, GlobalConstants.AdministratorRoleName);
+            }
+            else
+            {
+                throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+            }
+        }
+    }
+}
